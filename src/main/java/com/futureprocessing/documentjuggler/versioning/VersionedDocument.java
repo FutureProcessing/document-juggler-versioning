d4f23e63b5eadf051341898a7285eb5a9fdf7ea1
Bartosz Niziołek <bniziolek@future-processing.com>
2015-03-30 09:51:02 +0200

    Update to follow Document-Juggler

--- src/main/java/com/futureprocessing/documentjuggler/versioning/VersionedDocument.java
+++ src/main/java/com/futureprocessing/documentjuggler/versioning/VersionedDocument.java
@@ -1,22 +1,22 @@
 package com.futureprocessing.documentjuggler.versioning;
 
+import com.futureprocessing.documentjuggler.annotation.AsObjectId;
 import com.futureprocessing.documentjuggler.annotation.DbField;
-import com.futureprocessing.documentjuggler.annotation.ObjectId;
 
 import java.util.Date;
 
 public interface VersionedDocument<MODEL extends VersionedDocument> {
 
-    static final String DOC_ID = "_docId";
-    static final String VERSION = "_v";
-    static final String DATE = "_date";
-    static final String PENDING_ARCHIVE = "_pending_archive";
+    String DOC_ID = "_docId";
+    String VERSION = "_v";
+    String DATE = "_date";
+    String PENDING_ARCHIVE = "_pending_archive";
 
-    @ObjectId
+    @AsObjectId
     @DbField(DOC_ID)
     String getId();
 
-    @ObjectId
+    @AsObjectId
     @DbField(DOC_ID)
     MODEL withId(String id);
 
